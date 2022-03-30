533da9de9d3feb3c488a4ca7405f55811cceb8dd
NTHXXIV <ngtrunghieu9731@gmail.com>
2022-03-30 23:00:35 +0700

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PrimarySearchAppBar from "./PrimarySearchAppBar";
 import AppCard from "./AppCard";
 import Data from "../data";
